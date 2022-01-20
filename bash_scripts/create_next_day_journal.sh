5c11874b92cb3614f75a513c4e921ad06222d7de
Conrad Stansbury <chstansbury@gmail.com>
2022-01-20 12:30:58 -0800

    Plan an extra day ahead so I can kick things forward.

--- bash_scripts/create_next_day_journal.sh
+++ bash_scripts/create_next_day_journal.sh
@@ -2,5 +2,5 @@
 STARTDIR=`$(pwd)`
 TOML_DIR=/home/moose/conrad/GitHub/voice-notes
 cd "$TOML_DIR" || exit
-poetry run bash -c "which python"
 poetry run bash -c "python scripts/create_month_and_day_planning_page.py --day_delta=1 $* && cd $STARTDIR"
+poetry run bash -c "python scripts/create_month_and_day_planning_page.py --day_delta=2 $* && cd $STARTDIR"
